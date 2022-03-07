acf7e0d5251eba2fd0bdac6220949207fe1e8bce
Thiago Elias de Souza <TS104821@grpoesp.com.br>
2022-03-07 20:30:19 -0300

    titulo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,8 @@
 
        
         <meta name="description" content="" />
-        <meta name="author" content="FisicaLab - Resolução de questões de física e ciências exatas, problemas resolvidos e questões de vestibulares ENEM FUVEST e concursos públicos" />
-        <title>Resolução de questões de física e ciências exatas, problemas resolvidos e questões de vestibulares ENEM FUVEST e concursos públicos
+        <meta name="author" content="Resolução de questões de física e ciências exatas, problemas resolvidos e questões de vestibulares ENEM FUVEST e concursos públicos" />
+        <title>FisicaLab - Resolução de questões de física e ciências exatas, problemas resolvidos e questões de vestibulares ENEM FUVEST e concursos públicos
         </title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
