76457115608f4cef1c7bfa67f0104228a768c9fd
Thiago Elias de Souza <TS104821@grpoesp.com.br>
2022-05-30 14:45:53 -0300

    alt

--- resources/views/busca.blade.php
+++ resources/views/busca.blade.php
@@ -16,9 +16,9 @@
                             <!-- to get an API token!-->
                             <form class="form-subscribe" id="contactForm" data-sb-form-api-token="API_TOKEN" action="/busca" value="{{ csrf_token() }}">
                                 <!-- Email address input-->
-                                <div class="row">
+                                <div class="row justify-content-around">
                                     <div class="col">
-                                        <input class="form-control form-control-lg" id="campoBusca" type="text"  name="campoBusca" placeholder="Digite o texto aqui" data-sb-validations="required,text" />
+                                        <input class="form-control form-control-lg" id="campoBusca" type="text"    name="campoBusca" placeholder="Digite o texto aqui" data-sb-validations="required,text" />
                                        
                                     </div>
                                     <div class="col-auto"><button class="btn btn-primary btn-lg " id="submitButton" type="submit">Buscar</button></div>
@@ -27,18 +27,12 @@
                                 <!---->
                                 <!-- This is what your users will see when the form-->
                                 <!-- has successfully submitted-->
-                                <div class="d-none" id="submitSuccessMessage">
-                                    <div class="text-center mb-3">
-                                        <div class="fw-bolder">Form submission successful!</div>
-                                        <p>To activate this form, sign up at</p>
-                                        <a class="text-white" href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                    </div>
-                                </div>
+                                
                                 <!-- Submit error message-->
                                 <!---->
                                 <!-- This is what your users will see when there is-->
                                 <!-- an error submitting the form-->
-                                <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                                
                             </form>
                         </div>
                     </div>
